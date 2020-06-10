8f34996ba637582ef4a913edffe1ac05df7588f7
iagrg2001 <igarg2001@gmail.com>
2020-06-10 09:39:35 +0530

    Completed : 1, 2, 3 sections

---
+++ start/server/src/datasources/launch.js
@@ -0,0 +1,40 @@
+const { RESTDataSource } = require('apollo-datasource-rest')
+
+class LaunchAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://api.spacexdata.com/v2/'
+    }
+    launchReducer(launch) {
+        return {
+          id: launch.flight_number || 0,
+          cursor: `${launch.launch_date_unix}`,
+          site: launch.launch_site && launch.launch_site.site_name,
+          mission: {
+            name: launch.mission_name,
+            missionPatchSmall: launch.links.mission_patch_small,
+            missionPatchLarge: launch.links.mission_patch,
+          },
+          rocket: {
+            id: launch.rocket.rocket_id,
+            name: launch.rocket.rocket_name,
+            type: launch.rocket.rocket_type,
+          },
+        };
+      }
+
+    getAllLaunches = async () => {
+        const response = await this.get('launches');
+        //console.log(response)
+        return Array.isArray(response) ? response.map(launch => this.launchReducer(launch)) : []
+    }
+    getLaunchByID = async ({launchID}) => {
+        const response = await this.get('launches', {flight_number : launchID})
+        return this.launchReducer(response[0])
+    }
+
+    getLaunchesByIDs = ({launchIDs}) => {
+        return Promise.all(launchIDs.map(launchID => this.getLaunchByID({launchID})))
+    }
+}
+module.exports = LaunchAPI
---
+++ start/server/src/schema.js
@@ -0,0 +1,76 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+#schema goes here 
+
+type Query {
+
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+  
+  
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
+type Mutation {
+   bookTrips (launchIDs : [ID]!) : TripUpdateResponse!
+   cancelTrip (launchID : ID!) : TripUpdateResponse!
+   login (email : String) : String #login token
+}
+
+type TripUpdateResponse {
+   success : Boolean!
+   message : String
+   launches : [Launch]
+}
+
+type Launch {
+   id : ID!
+   site : String
+   mission : Mission
+   rocket : Rocket
+   isBooked : Boolean!
+}
+
+type Mission {
+   name : String
+   missionPatch(size : PatchSize): String
+}
+
+type Rocket {
+   id: ID!
+   name : String
+   type : String
+}
+
+type User {
+   id : ID!
+   email : String!
+   trips : [Launch]!
+}
+
+enum PatchSize {
+   SMALL
+   LARGE
+}
+`;
+module.exports = typeDefs
